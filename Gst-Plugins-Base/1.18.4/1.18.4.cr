36501f509026f9316ddac21056c5929a784928c3
Fulgurance <zohran.londais@gmail.com>
2023-04-16 23:58:18 +0100

    Added Alsa-Lib, gst plugins and Libsndfile

---
+++ Gst-Plugins-Base/1.18.4/1.18.4.cr
@@ -0,0 +1,31 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+
+        fileReplaceTextAtLineNumber("#{mainWorkDirectoryPath(false)}/gst-libs/gst/gl/meson.build",")","implicit_include_directories : false)",1005)
+    end
+
+    def configure
+        super
+
+        runMesonCommand([   "--prefix=/usr",
+                            "--buildtype=release",
+                            "--wrap-mode=nodownload"],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+end
