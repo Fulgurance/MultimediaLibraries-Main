0239fd6dc3bd6d66e34d46ac7d2d6a5f1ddaeae4
Fulgurance <zohran.londais@gmail.com>
2023-11-29 14:22:54 +0000

    Added new software

---
+++ Libvpx/1.13.0/1.13.0.cr
@@ -0,0 +1,32 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        @buildDirectoryName = "libvpx-build "
+        super
+
+        fileReplaceTextAtLineNumber("#{mainWorkDirectoryPath(false)}/build/make/Makefile","cp -p","cp",295)
+    end
+
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--enable-shared",
+                            "--disable-static"],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
